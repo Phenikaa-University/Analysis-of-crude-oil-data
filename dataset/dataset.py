881d679f91df9ef5cb807344ae161ffb3fccfd60
Cuong Vuong <cngvng123@gmail.com>
2024-04-28 15:22:31 +0700

    update preprocess dataset using stl-sr transform

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -66,3 +66,39 @@ class FRED():
         plt.legend()
         plt.savefig(f"{save_path_plot}/{self.symbol}_residual.png")
         plt.show()
+    
+    def _get_score_sr(self):
+        xs, y_res = self._get_residual_com()
+        scores = generate_spectral_score(y_res.tolist())
+        return xs, scores
+    
+    def _plot_fred_dataset_score_sr(self, save_path_plot):
+        xs, scores = self._get_score_sr()
+        plt.figure(figsize=(12,6))
+        plt.plot(xs, scores, label=self.symbol)
+        plt.xlabel('Date')
+        plt.ylabel('Value')
+        plt.title(f"BRENT crude oil prices from FRED about {self.symbol} with spectral residual score")
+        plt.legend()
+        plt.savefig(f"{save_path_plot}/{self.symbol}_score_sr.png")
+        plt.show()
+        
+    def _stl_sr_transform(self, slide_window: int):
+        xs, score = self._get_score_sr()
+        y_df = pd.DataFrame({self.symbol: score + slide_window}, index=xs)
+        y_decom = seasonal_decompose(y_df, model='multiplicative')
+        y_trans = np.copy(y_decom.resid)
+        # Padding missing "nan" datapoints with 1 at the beginning and end of residual sequence y_res
+        y_trans[0:2]=1
+        y_trans[y_trans.shape[0]-2:y_trans.shape[0]]=1
+        return xs, y_trans
+    
+    def _plot_fred_dataset_stl_sr_transform(self, slide_window: int, save_path_plot):
+        xs, y_trans = self._stl_sr_transform(slide_window)
+        plt.figure(figsize=(12,6))
+        plt.plot(xs, y_trans, label=self.symbol)
+        plt.xlabel('Date')
+        plt.ylabel('Value')
+        plt.title(f"BRENT crude oil prices from FRED about {self.symbol} with STL-SR transform: {slide_window} window")
+        plt.legend()
+        plt.savefig(f"{save_path_plot}/{self.symbol}_stl_sr_transform.png")
