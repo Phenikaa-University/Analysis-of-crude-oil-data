0c25bdfc9c690a2b74a1a39e25011527441a8d5a
Cuong Vuong <cngvng123@gmail.com>
2024-05-17 18:00:00 +0700

    update models

--- models/cnnAD.py
+++ models/cnnAD.py
@@ -5,20 +5,19 @@ from tensorflow.keras.models import Sequential # type: ignore
 from tensorflow.keras.layers import Dense, Conv1D, Flatten, MaxPooling1D, Dropout # type: ignore
 
 class CNNAnomalyDetector:
-    def __init__(self, window:int = 100, n_features: int = 1, num_filt_1: int = 16, num_filt_2: int = 16, num_nrn_dl: int = 20, num_nrn_ol: int = 1,
-                 kernel_size: int = 2, conv_strides: int = 1, pool_size_1: int = 2, pool_size_2: int = 2, dropout_rate: float = 0.1):
+    def __init__(self, opt):
         super().__init__()
-        self.window = window
-        self.n_features = n_features
-        self.num_filt_1 = num_filt_1
-        self.num_filt_2 = num_filt_2
-        self.num_nrn_dl = num_nrn_dl
-        self.num_nrn_ol = num_nrn_ol
-        self.kernel_size = kernel_size
-        self.conv_strides = conv_strides
-        self.pool_size_1 = pool_size_1
-        self.pool_size_2 = pool_size_2
-        self.dropout_rate = dropout_rate
+        self.window = opt.window_size
+        self.n_features = opt.n_features
+        self.num_filt_1 = opt.num_filt_1
+        self.num_filt_2 = opt.num_filt_2
+        self.num_nrn_dl = opt.num_nrn_dl
+        self.num_nrn_ol = opt.num_nrn_ol
+        self.kernel_size = opt.kernel_size
+        self.conv_strides = opt.conv_strides
+        self.pool_size_1 = opt.pool_size_1
+        self.pool_size_2 = opt.pool_size_2
+        self.dropout_rate = opt.dropout_rate
         self.model = self._build_cnn_model()
         self.loss = []
 
@@ -43,21 +42,21 @@ class CNNAnomalyDetector:
         model.add(Dense(units=self.num_nrn_ol))
         return model
 
-    def train(self, batch_sample, batch_label, num_epochs, batch_size, learning_rate, save_path, stl_sr=True):
-        optimizer = tf.keras.optimizers.Adam(learning_rate=learning_rate)
+    def train(self, batch_sample, batch_label, opt):
+        optimizer = tf.keras.optimizers.Adam(learning_rate=opt.lr)
         self.model.compile(optimizer=optimizer, loss='mean_absolute_error')
-        self.model.fit(batch_sample, batch_label, epochs=num_epochs, batch_size=batch_size)
-        if stl_sr:
-            self.model.save_weights(f'{save_path}/cnn_stl_sr.weights.h5')
-        else:
-            self.model.save_weights(f'{save_path}/cnn_sr.weights.h5')
+        self.model.fit(batch_sample, batch_label, epochs=opt.num_epochs, batch_size=opt.batch_size)
+        if opt.mode == 'stl_sr':
+            self.model.save_weights(f'{opt.checkpoint_dir}cnn_stl_sr.weights.h5')
+        elif opt.mode == "stl":
+            self.model.save_weights(f'{opt.checkpoint_dir}cnn_sr.weights.h5')
     def predict(self, batch_sample):
         return self.model.predict(batch_sample, verbose=1)
     
-    def detect_anomaly(self, batch_sample, threshold):
+    def detect_anomaly(self, ys_cor, batch_sample, threshold):
         prediction = self.predict(batch_sample)
-        y_pred = prediction.reshape(prediction[self.window:].shape)
-        y_true = prediction[self.window:].astype('float32')
+        y_pred = prediction.reshape(ys_cor[self.window:].shape)
+        y_true = ys_cor[self.window:].astype('float32')
         dist = (y_true - y_pred) ** 2
         anomalies = np.where(dist > threshold)[0] * self.window
         return anomalies
