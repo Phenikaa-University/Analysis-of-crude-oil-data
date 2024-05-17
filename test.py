6ec4a90e2dea8518ae6f595efd6b3b7154080859
Cuong Vuong <cngvng123@gmail.com>
2024-05-17 16:39:22 +0700

    update main train and test

---
+++ test.py
@@ -0,0 +1,47 @@
+from dataset.dataset import FREDtest, FREDtrain
+import argparse
+from models.cnnAD import CNNAnomalyDetector
+
+def get_opt():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--date_time', type=str, required=True)
+    parser.add_argument('--symbol', type=str, required=True)
+    
+    parser.add_argument('--lr', type=float, default=0.00005)
+    parser.add_argument('--dropout_rate', type=float, default=0.1)
+    parser.add_argument('--batch_size', type=int, default=32)
+    parser.add_argument('--num_epochs', type=int, default=100)
+    parser.add_argument('--checkpoint_dir', type=str, default='./checkpoints/')
+    parser.add_argument('--mode', type=str, default='stl_sr')
+    
+    parser.add_argument('--window_size', type=int, default=100)
+    parser.add_argument('--slide_window', type=int, default=20)
+    parser.add_argument('--pred_window', type=int, default=1)
+    
+    parser.add_argument('--n_features', type=int, default=1)
+    parser.add_argument('--num_filt_1', type=int, default=16)
+    parser.add_argument('--num_filt_2', type=int, default=16)
+    parser.add_argument('--num_nrn_dl', type=int, default=20)
+    parser.add_argument('--num_nrn_ol', type=int, default=1)
+    parser.add_argument('--kernel_size', type=int, default=2)
+    parser.add_argument('--conv_strides', type=int, default=1)
+    parser.add_argument('--pool_size_1', type=int, default=2)
+    parser.add_argument('--pool_size_2', type=int, default=2)
+    
+    
+    opt = parser.parse_args()
+    return opt
+    
+def main():
+    opt = get_opt()
+    print(opt)
+    
+    test_data = FREDtest(opt)
+    batch_sample, batch_label, ys_corr, postitions = test_data.load_data(opt)
+    model = CNNAnomalyDetector(opt)
+    model.load_weights("checkpoints/cnn_stl_sr.weights.h5")
+    detected = model.predict(batch_sample)
+    
+
+if __name__ == '__main__':
+    main()
